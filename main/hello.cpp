ed49e04167512b2713b71b0a421f9bc22e7ea5a0
Vijju <vijjuhavaldar@gmail.com>
2025-03-11 21:42:58 +0530

    changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<< "Thankyou";
+  cout<< "Thankyou \n";
+  cout<< "I am Vijju";
   return 0;
 }
